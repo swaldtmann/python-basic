bd571b48e6d30617a66eec704f98af8cccb459ae
Stephan Waldtmann <stephan@waldtmann.de>
2018-11-04 22:48:16 +0100

    Pylint on fact.py

--- Module/fact.py
+++ Module/fact.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+"""
+Author:
+"""
 
 def fact(n):
+    """ Fakultät des Parameter n
+    """
     return 1 if n == 1 else n * fact(n-1)
 
 
-if (__name__ == '__main__'):
+if __name__ == '__main__':
     import sys
     if len(sys.argv) > 1:
         print(fact(int(sys.argv[1])))
