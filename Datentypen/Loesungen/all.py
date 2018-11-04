715575b4d5c545ed8cd74a2dff6c50d859751164
Stephan Waldtmann <stephan@waldtmann.de>
2018-11-04 13:04:16 +0100

    More

--- Datentypen/Loesungen/all.py
+++ Datentypen/Loesungen/all.py
@@ -1,4 +1,37 @@
-# show all datatypes
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
 
 
-#???
+# Zahl
+i = 42
+print(i)
+
+r = 3.14
+print(r)
+
+x = 2 + 3j
+print(x)
+
+# String
+s = "Zeichenkette"
+print(s)
+
+# List
+l = ["Äpfel", "Birnen", "Pflaumen"]
+print(l)
+
+# Tupel
+t = ("t", "wie", "Tupel")
+print (t)
+
+# Sets
+se = {'Rhein', 'Main', 'Werra', 'Weser'}
+print(se)
+
+# Dictionaries
+dic = {"de": "Deutsch", "fr": "Französisch", "es": "Spanisch"}
+print(dic)
+
+# Boleans
+b = True
+print(b)
